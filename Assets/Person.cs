4e4d8c4c43b0248ffc544de0f207a1ee6c1bfc41
Eslam Hossam <eslamhossam.tech@gmail.com>
2024-12-14 01:27:18 +0200

    optimize the collider of boxes & fix score increment issue & add santa's car and some christmas trees

--- Assets/Person.cs
+++ Assets/Person.cs
@@ -8,14 +8,32 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Person : MonoBehaviour
 {
-    private int giftCount = 0; // Keeps track of collected gifts
+    public static int giftCount = 0; // Keeps track of collected gifts
+    private AudioSource audioSource;
+   // private Person personScript; //
+    private Collider boxCollider; // Reference to the box collider
 
+    void Start()
+    {
+        // Get the AudioSource component attached to this GameObject
+        audioSource = GetComponent<AudioSource>();
+
+       // personScript = GetComponent<Person>();
+        boxCollider = GetComponent<Collider>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Gift")) // Ensure the object has the "Gift" tag
         {
+
             // Disable XR Grab Interaction for the gift
+               boxCollider.enabled = false; // Disable the collider of the box
             DisableGrabInteraction(other.gameObject);
+
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
 
             // Find the Canvas by name
             GameObject canvas = GameObject.Find("Canvas");
@@ -27,7 +45,13 @@ public class Person : MonoBehaviour
 
                 if (progressBar != null)
                 {
+                    
                     progressBar.fillAmount += 0.1f;
+
+                    //if (progressBar.fillAmount > 1f)
+                    //{
+                    //    progressBar.fillAmount = 1f;
+                    //}
                 }
                 else
                 {
@@ -41,8 +65,15 @@ public class Person : MonoBehaviour
                 if (textTMP != null)
                 {
                     // Increase gift count and update the text
+                    Debug.Log("1 before increment, gift count = " + giftCount.ToString());
                     giftCount++;
+                    Debug.Log("2 before concatination , gift count = " + giftCount.ToString());
+                    //if (giftCount > 10)
+                    //{
+                    //    giftCount = 10;
+                    //}
                     textTMP.text = "10/" + giftCount.ToString(); // Update the TMP text to display the new count
+                    Debug.Log(" result text =" + textTMP.text);
                 }
                 else
                 {
@@ -56,12 +87,15 @@ public class Person : MonoBehaviour
 
             Debug.Log("Gift Collected!");
         }
+
+        //personScript.enabled = false; // Disable the script
     }
 
     private void DisableGrabInteraction(GameObject gift)
     {
         // Disable the XR Grab Interaction component
         var grabInteraction = gift.GetComponent<XRGrabInteractable>();
+
         if (grabInteraction != null)
         {
             grabInteraction.enabled = false; // Disables the grab interaction
@@ -70,5 +104,8 @@ public class Person : MonoBehaviour
         {
             Debug.LogWarning("XR Grab Interaction component not found on the gift!");
         }
+
     }
+
+    
 }
